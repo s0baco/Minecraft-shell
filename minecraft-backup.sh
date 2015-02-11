def14537d90f850867b350ac9d83d459594c8e1f
s0baco (そば娘) <s0baco.777@gmail.com>
2015-02-11 14:14:01 +0900

    Update minecraft-backup.sh
    更新日時が $PERIOD 日前のバックアップデータを削除するようにした。
    
    多分動くはず。

--- minecraft-backup.sh
+++ minecraft-backup.sh
@@ -2,27 +2,31 @@
 
 . ./minecraft.conf
 
-screen -S ${SESSION_NAME} -p 0 -X eval 'stuff "say ${HEADER} WORLD BACKUP START.\015"'
-screen -S ${SESSION_NAME} -p 0 -X eval 'stuff "save-all\015"'
+BACKUP_DATE=`date "+%Y%m%d%H%M"`
+
+screen -S $SESSION_NAME -p 0 -X eval 'stuff "say $HEADER WORLD BACKUP START.\015"'
+screen -S $SESSION_NAME -p 0 -X eval 'stuff "save-all\015"'
 
 # mkdir
-mkdir ${TEMP_DIR}
+mkdir $TEMP_DIR
 
-if [ ! -d ${BACKUP_DIR} ]
+if [ ! -d $BACKUP_DIR ]
 then
-	mkdir ${BACKUP_DIR}
+	mkdir $BACKUP_DIR
 fi
+
 # copy
-cp -Rf ${COPY_DIR} ${TEMP_DIR}
+cp -Rf $COPY_DIR $TEMP_DIR
 
 # cd
-cd ${BACKUP_DIR}
+cd $BACKUP_DIR
 
 # zip
-zip -r world_$BACKUP_DATE.zip ${TEMP_DIR}
+zip -r world_$BACKUP_DATE.zip $TEMP_DIR
 
-# del temp
-rm -rf ${TEMP_DIR}
+# delete
+rm -rf $TEMP_DIR
+find $BACKUP_DIR -type f -mtime $PERIOD -exec rm -f '{}' ';'
 
-screen -S ${SESSION_NAME} -p 0 -X eval 'stuff "say $HEADER WORLD BACKUP COMPLETE.\015"'
+screen -S $SESSION_NAME -p 0 -X eval 'stuff "say $HEADER WORLD BACKUP COMPLETE.\015"'
 
