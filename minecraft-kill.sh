cf6bd9cac1be2f83fe842efba1f3ab139560bfbd
s0baco <s0baco.777@gmail.com>
2015-02-11 23:30:13 +0900

    Update stop.sh
    ウェイト無しのkill.shとウェイト有りのstop.shにした

--- minecraft-kill.sh
+++ minecraft-kill.sh
@@ -4,15 +4,6 @@
 
 screen -S $SESSION_NAME -p 0 -X eval 'stuff "say $HEADER SERVER STOPPING...\015"'
 
-i=0
-while [ "$i" -le "$WAIT" ]
-do
-	h=$WAIT-$i
-	screen -S $SESSION_NAME -p 0 -X eval 'stuff "say $HEADER $h...\015"'
-	echo "$h..."
-	sleep 1
-done
-
 screen -S $SESSION_NAME -p 0 -X eval 'stuff "save-all\015"'
 screen -S $SESSION_NAME -p 0 -X eval 'stuff "say $HEADER WORLD SAVING...\015"'
 
